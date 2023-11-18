85349c5f7ca7e6655f5c2e59d6a0a1ae9f06f642
slavio69 <85609004+slavio69@users.noreply.github.com>
2023-11-18 21:07:05 +0100

    adding 2 numbers end result via "=" button working

--- script.js
+++ script.js
@@ -14,14 +14,15 @@ const buttonNames = [
     '7',
     '8',
     '9',
-    '',
+    ',',
     '0',
-    ''
+    '.'
 ]
 
 let number1 = '';
 let number2 = '';
 let operator = '';
+let result = 0;
 const display = document.querySelector('#display')
 /*display.textContent = `${number1}`;*/
 
@@ -33,18 +34,8 @@ function createButtons() {
         button.setAttribute('class', 'buttonsAll')
         button.setAttribute('value', buttonNames[i])
         button.textContent = buttonNames[i];
-        if(button.getAttribute('value')=='='){
-            button.addEventListener('click', () => {
-                getResult(number1, number2, operator)
-            })   
-        } else if(parseInt(button.getAttribute('value') == NaN)){
-        button.addEventListener('click', () => {
-            setOperator(button.getAttribute('value'));
-        })} else {
-            button.addEventListener('click', () => {
-                number1 += button.getAttribute('value');
-            })
-        }
+        const buttonValue = button.getAttribute('value')
+        addListener(button, buttonValue)
         container.appendChild(button);
 
     } 
@@ -52,16 +43,70 @@ function createButtons() {
 
 
 function setOperator(value) {
-    console.log(value)
+    if (number1=='') {
+        alert("Pick a Number first!")
+    }
     operator = value;
-    /*display.textContent = `${operator}`;*/
-    number2 = number1;
-    number1 = '';
+    result = parseFloat(number1)
+    display.textContent = 0;
 }
 
 function getResult(num1, num2, operator) {
-    console.log(num1, num2, operator)
+    if (operator=='+'){
+        result = parseFloat(num1) + parseFloat(num2)
+    }else if (operator=='-'){
+        result = parseFloat(num1) - parseFloat(num2)
+    }else if (operator=='*'){
+        result = parseFloat(num1) * parseFloat(num2)
+    }else if (operator=='/'){
+        result = parseFloat(num1) / parseFloat(num2)
+    }
+    display.textContent = `${result}`
 }
 
+function selecteNumber(value) {
+    if (operator === '') {
+        number1 += value;
+        display.textContent = `${number1}`
+    } else {
+        number2 += value;
+        display.textContent = `${number2}`
+    }
+    
+}
+
+function clear() {
+    number1 = '';
+    number2 = '';
+    operator = '';
+    result = 0;
+    display.textContent = 0
+}
+
+
+function addListener(button, value) {
+    if ( value == '.' || value == ',') {
+        button.addEventListener('click', () => {
+            selecteNumber(value)
+        }) 
+    }
+    else if ( value == 'C') {
+        button.addEventListener('click', () => {
+            clear()
+        }) 
+    }
+    else if(value =='='){
+        button.addEventListener('click', () => {
+            getResult(number1, number2, operator)
+        })   
+    } else if(value=='+' || value == '-' || value == '*' || value == '/'){
+    button.addEventListener('click', () => {
+        setOperator(button.getAttribute('value'));
+    })} else {
+        button.addEventListener('click', () => {
+            selecteNumber(value);
+        })
+    }
+}
 
 createButtons()
